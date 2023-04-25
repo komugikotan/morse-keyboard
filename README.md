5c1386dc368dbb8dcb56e46335d9b6e525e9687c
komugikotan <komugikotan1@gmail.com>
2023-04-26 07:11:42 +0900

    first_release

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# morse-keyboard
+# Morse-keyboard
+## 概要
+
+**ENGLISH VERSION IS [HERE](README-EN.md)!!!**
+
+Arduinoを使用し、電鍵をキーボード化するソフトウェアです。Arduinoに接続されたボタンまたは電鍵など（ONとOFFの切り替えができるスイッチであればなんでも可）を入力装置とし、モールス信号（CW）をキーボード入力に変換します。スピードなどは、コード内で切り替えることができます。
+
+## インストール
+
+本プログラムを動作させるには、次の4つのことが必要です。
+
+1. telegraph.inoのArduino Unoへの書き込み
+2. Arduino Unoのデジタルピン２にボタンや電鍵を接続
+3. Pythonで必要なライブラリをインストール
+4. Pythonプログラムを起動
+
+## PC側
+
+本プログラムは、Arduino IDE及びPython 3.11を使用し作成されました。Arduinoのコードは、Arduino IDEを用いてArduino Unoに書き込んでください。特に必要なライブラリ等はありません。また、PythonプログラムはPySerialライブラリ、Pyperclipライブラリ、Pyautoguiライブラリのインストールが必要です。
+
+```
+pip install pyautogui
+pip install pyserial
+pip install pyperclip
+```
+
+プログラム内の１５行目の「COM4」は、環境に合わせて変更してください。数字の部分は、Arduino IDE内から確認が可能です。写真を参照してください。
+
+## Arduino Uno側
+
+検証はArduino Unoでのみ行っております。他のArduinoは未検証です。
+
+Arduino Unoのデジタルピン2ピンに、電鍵またはタクトスイッチなどを接続してください。ピン番号は、telegraph.inoの一行目で指定してください。デフォルトは２に設定されています。
+
+
+![Arduino-IDE](/arduino-ide.jpg) 
+
+## 速度変更
+
+モールスの長点の長さを変更したい場合は、Receiver.pyの６行目の「speed」変数を変更してください。これは、長点の長さを秒数で入力してください。また、７行目の「space_speed」変数は、空白の長さ（語と語のスペース）です。これも、空白の長さを秒数で入力してください。
+
+## モールスの和文・欧文切り替え
+
+設定を変更することで、和文モード・欧文モードと切り替えることができます。切り替えるには、Receiver.pyの12行目の「type」変数を変更してください。値を1に設定すると、欧文モードになり、2に設定すると和文モードになります。
---
+++ receiver.py
@@ -0,0 +1,206 @@
+import serial
+import time
+import pyautogui
+import pyperclip
+
+#change -'s speed your own
+speed = 0.3
+space_speed = 0.7
+
+#Morse type (Japanese or Roman)
+#If you want Roman morse, you set variable "type" to 1. If you want Japanese morse, you set variable "type" to 2.
+type = 1
+
+status = 2
+morse = ""
+morse_output = ""
+blank = ""
+blank_s = ""
+tontu = ""
+tontu_s = ""
+
+
+def type_nihongo(letters):
+    pyperclip.copy(letters)
+    pyautogui.hotkey('ctrl', 'v')
+
+english_morse_dict = {
+    ".-":"a",
+    "-...":"b", 
+    "-.-.":"c",
+    "-..":"d",
+    ".":"e", 
+    "..-.":"f",
+    "--.":"g",
+    "....":"h", 
+    "..":"i",
+    ".---":"j",
+    "-.-":"k", 
+    ".-..":"l",
+    "--":"m",
+    "-.":"n", 
+    "---":"o",
+    ".--.":"p",
+    "--.-":"q", 
+    ".-.":"r",
+    "...":"s",
+    "-":"t", 
+    "..-":"u",
+    "...-":"v",
+    ".--":"w", 
+    "-..-":"x",
+    "-.--":"y",
+    "--..":"z", 
+    ".----":"1",
+    "..---":"2",
+    "...--":"3", 
+    "....-":"4",
+    ".....":"5",
+    "-....":"6", 
+    "--...":"7",
+    "---..":"8",
+    "----.":"9", 
+    "-----":"0",
+    ".-.-.-":".",
+    "--..--":",",
+    "---...":":", 
+    "..--..":"?",
+    "..--.-":"_",
+    ".-.-.":"+", 
+    "-....-":"-",
+    "......":"^", 
+    "-..-.":"/"
+}
+
+japanese_morse_dict = {
+    ".----":"1",
+    "..---":"2",
+    "...--":"3", 
+    "....-":"4",
+    ".....":"5",
+    "-....":"6", 
+    "--...":"7",
+    "---..":"8",
+    "----.":"9", 
+    "-----":"0",
+    ".-":"イ", 
+    ".-.-":"ロ",
+    "-...":"ハ",
+    "-.-.":"ニ", 
+    "-..":"ホ",
+    ".":"ヘ",
+    "..-..":"ト", 
+    "..-.":"チ",
+    "--.":"リ",
+    "....":"ヌ", 
+    "-.--.":"ル",
+    ".---":"ヲ",
+    "-.-":"ワ", 
+    ".-..":"カ",
+    "--":"ヨ",
+    "-.":"タ", 
+    "---":"レ",
+    "---.":"ソ",
+    ".--.":"ツ", 
+    ".--.":"ネ",
+    ".-.":"ナ",
+    "...":"ラ",
+    "-":"ム", 
+    "..-":"ウ",
+    ".-..-":"ヰ",
+    "..--":"ノ", 
+    ".-...":"オ",
+    "...-":"ク", 
+    ".--":"ヤ",
+    ".--.":"マ",
+    "-.--":"ケ",
+    "--..":"フ",
+    "----":"コ", 
+    "-.---":"エ",
+    ".-.--":"テ",
+    "--.--":"ア", 
+    "-.-.-":"サ",
+    "-.-..":"キ", 
+    ".--..":"ユ",
+    ".---.":"メ",
+    "..-.-":"ミ", 
+    "--.-.":"シ",
+    ".--..":"ヱ",
+    "--..-":"ヒ",
+    "-..-.":"モ",
+    ".---.":"セ", 
+    "---.-":"ス",
+    ".-.-.":"ン",
+    "..":"゛", 
+    "..--.":"゜",
+    ".--.-":"ー", 
+    ".-.-..":"、"
+}
+
+if (type == 1):
+    morse_dict = english_morse_dict
+else:
+    morse_dict = japanese_morse_dict
+
+while(True):
+    ser = serial.Serial('COM6', 9600) 
+    not_used = ser.readline()
+
+    while True:
+        val_decoded = ser.readline()
+
+        if b'click' in val_decoded:     
+            status = 1
+            tontu = time.perf_counter()
+            print("you pressed")
+
+            print(morse_output)
+            print(morse)
+            
+            try:
+                blank_s = time.perf_counter()
+                blank_time = blank_s - blank
+
+                
+
+                if blank_time > speed:
+                    try:
+                        type_letter = morse_dict[morse]
+
+                        morse_output = morse_output + type_letter
+
+                        if (type == 2):
+                            type_nihongo(type_letter) #Pyautoguiは日本語が入力できないためコピー＆ペーストで対処
+                            morse = ""
+                        else:
+                            pyautogui.write(type_letter)
+                            morse = ""
+                    except:
+                        morse_output = morse_output + "*"
+                        pyautogui.write('*')
+                        morse = ""
+
+                if blank_time > space_speed:
+                    morse_output = morse_output + " "
+                    pyautogui.write(' ')
+                    morse = ""
+
+            except:
+                pass
+    
+        else:
+            if status == 1:
+                tontu_s = time.perf_counter()
+                pressed_time = tontu_s - tontu
+                if pressed_time > speed:
+                    morse = morse + "-"
+                if pressed_time < speed:
+                    morse = morse + "."
+
+                blank = time.perf_counter()
+                
+                status = 0
+
+    ser.close()
+
+    
